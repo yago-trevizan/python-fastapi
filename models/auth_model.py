1612db6b5402bc0a7cd85be6649cab26decc91cf
Yago Trevizan de Araujo <yago.ta@hotmail.com>
2026-08-16 11:10:03 -0300

    feat: create new models at auth_model.py

--- models/auth_model.py
+++ models/auth_model.py
@@ -1,5 +1,17 @@
 from pydantic import BaseModel, Field
 
-class Credentials(BaseModel):
-  username: str
+class User(BaseModel):
+  name: str = Field(min_length=3)
+  username: str = Field(max_length=12)
   password: str = Field(min_length=6)
+
+class Credentials(BaseModel):
+  username: str = Field(max_length=12)
+  password: str = Field(min_length=6)
+
+class UserOutput(BaseModel):
+  name: str
+  username: str
+
+class GetUsersOutput(BaseModel):
+  users: list[UserOutput]
