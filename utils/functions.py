9bd07b0db6c1cae9ed67f25bd411a5bd8d8e4cd9
Yago Trevizan de Araujo <yago.ta@hotmail.com>
2026-08-20 15:33:26 -0300

    feat: add `verify_password` and `hash_password` functions

--- utils/functions.py
+++ utils/functions.py
@@ -1,4 +1,7 @@
 from db import tarefas
+from pwdlib import PasswordHash
+
+password_hash = PasswordHash.recommended()
 
 def get_next_id() -> int:
   next_id = 1
@@ -7,3 +10,10 @@ def get_next_id() -> int:
     next_id = tarefas[-1]["id"] + 1
 
   return next_id
+
+def hash_password(password: str):
+  return password_hash.hash(password)
+
+def verify_password(password: str, hashed_password: str):
+  return password_hash.verify(password, hashed_password)
+
