3cc1bb973b8b9751d26a858ffe10e844e525fe23
Yago Trevizan de Araujo <yago.ta@hotmail.com>
2026-08-21 18:18:05 -0300

    feat: create `verify_token` dependency and use at `users_router`

--- routes/users_router.py
+++ routes/users_router.py
@@ -1,33 +1,18 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 from typing import Annotated
 from models.user_model import GetUsersOutput
 from utils.db import usuarios
-from utils.dependencies import oauth2_scheme
-import jwt
-from os import getenv
-
-SECRET_KEY = getenv("SECRET_KEY")
-ALGORITHM = getenv("ALGORITHM")
+from utils.dependencies import verify_token
 
 user_router = APIRouter(prefix="/users", tags=["Usuários"])
 
 @user_router.get("/me")
-def me(token: Annotated[str, Depends(oauth2_scheme)]):
-  try:
-    decoded = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-  except jwt.ExpiredSignatureError:
-    raise HTTPException(status_code=401, detail="Token expired")
-
-  username = decoded["sub"]
+def me(decoded_token: Annotated[str, Depends(verify_token)]):
+  username = decoded_token["sub"]
   current_user = next((u for u in usuarios if u.username == username), None)
 
   return { "current_user": current_user }
 
-@user_router.get("/", response_model=GetUsersOutput)
-def get_users(token: Annotated[str, Depends(oauth2_scheme)]):
-  try:
-    jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-  except jwt.ExpiredSignatureError:
-    raise HTTPException(status_code=401, detail="Token expired")
-  
+@user_router.get("/", response_model=GetUsersOutput, dependencies=[Depends(verify_token)])
+def get_users():
   return { "users": usuarios }
--- utils/dependencies.py
+++ utils/dependencies.py
@@ -1,3 +1,23 @@
+from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
+from typing import Annotated
+from dotenv import load_dotenv
+from os import getenv
+import jwt
+
+load_dotenv()
+
+SECRET_KEY = getenv("SECRET_KEY")
+ALGORITHM = getenv("ALGORITHM")
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+def verify_token(token: Annotated[str, Depends(oauth2_scheme)]):
+  try:
+    decoded_token = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+  except jwt.ExpiredSignatureError:
+    raise HTTPException(status_code=401, detail="Expired Token")
+  except jwt.DecodeError:
+    raise HTTPException(status_code=401, detail="Invalid Token")
+  else:
+    return decoded_token
