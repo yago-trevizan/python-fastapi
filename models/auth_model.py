bc28e497c4c2456026ce3ef390de17ea4a34a71a
Yago Trevizan de Araujo <yago.ta@hotmail.com>
2026-08-25 11:44:08 -0300

    refactor: delete `Credentials` model

--- models/auth_model.py
+++ models/auth_model.py
@@ -1,9 +1,5 @@
 from pydantic import BaseModel, Field
 
-class Credentials(BaseModel):
-  username: str = Field(max_length=12)
-  password: str = Field(min_length=6)
-
 class SigninInput(BaseModel):
   name: str = Field(min_length=3)
   username: str = Field(max_length=12)
