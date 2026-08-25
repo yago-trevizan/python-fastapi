a548facd542768472f8589bf18c3ddf208832155
Yago Trevizan de Araujo <yago.ta@hotmail.com>
2026-08-25 10:01:55 -0300

    refactor: change functions names at `user_router`

--- routes/user_router.py
+++ routes/user_router.py
@@ -7,12 +7,12 @@ from utils.dependencies import verify_token
 user_router = APIRouter(prefix="/users", tags=["users"])
 
 @user_router.get("/me")
-def me(decoded_token: Annotated[str, Depends(verify_token)]):
+def get_current_user(decoded_token: Annotated[str, Depends(verify_token)]):
   username = decoded_token["sub"]
   current_user = next((u for u in users if u.username == username), None)
 
   return { "current_user": current_user }
 
 @user_router.get("/", response_model=GetUsersOutput, dependencies=[Depends(verify_token)])
-def get_users():
+def list_users():
   return { "users": users }
